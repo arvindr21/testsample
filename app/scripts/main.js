f2d81b65f269462c1241a74f033d675591eb7b70
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-02-23 21:43:53 +0530

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global movieManager, $*/
+
+
+window.movieManager = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    movieManager.init();
+});
